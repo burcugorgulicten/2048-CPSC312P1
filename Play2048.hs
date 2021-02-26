695a9a307aa39f59beaf067ceee0482a3556b7b2
Allison <allisonvearle@gmail.com>
2021-02-25 22:16:23 -0800

    Tidy file

--- Play2048.hs
+++ Play2048.hs
@@ -12,63 +12,6 @@ import System.Random
 import Data.List
 import System.Directory
 
--- starting board with no tiles
-emptyboard = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-
--- initboard creates a new board with 2 random tiles
-initboard :: IO [[Integer]]
-initboard = 
-    do
-        num <- randomRIO (1, 2 :: Integer)
-        return (addTile (addTile emptyboard 1) num)
-
--- initboardchallenge board n m s creates a new challenge board
--- with n normal tiles, m movable tiles, and s static tiles 
-initboardchallenge :: [[Integer]] -> [Integer] -> Integer -> Integer -> IO [[Integer]]
-initboardchallenge board n m s 
-    | n /= [] = initboardchallenge (addTile board (head n)) (tail n) m s
-    | m > 0 = initboardchallenge (addTile board (-1)) n (m-1) s
-    | s > 0 = initboardchallenge (addTile board (-2)) n m (s-1)
-    | otherwise = return board
-
--- display b prints the board with proper spacing
-display :: [[Integer]] -> String
-display b = "\n" ++ foldr (\ x y -> foldr displaynum "" x ++ "\n" ++ y) "\n" b
-
-displaynum :: Integer -> String -> String
-displaynum x y
-    | x == 0 = "*    " ++ y
-    | x < 10 = show x ++ "    " ++ y
-    | x < 100 = show x ++ "   " ++ y
-    | x < 1000 = show x ++ "  " ++ y
-    | otherwise = show x ++ " " ++ y
-
-loadDict :: IO (Dict [Char] Integer)
-loadDict =
-    do
-        scoresExist <- doesFileExist "scores.txt"
-        if scoresExist
-            then do
-                contents <- readFile "scores.txt"
-                let pairs = makepairs (lines contents)
-                return (foldr (uncurry insertval) emptyDict pairs)
-            else
-                return emptyDict
-
-makepairs :: [[Char]] -> [([Char], Integer)]
-makepairs [] = []
-makepairs (k:v:t) = (k, read v) : makepairs t
-
-saveDict :: Dict [Char] Integer -> IO ()
-saveDict dict = writeFile "scores.txt" (foldr (\ (k,v) y -> k++"\n"++show v++"\n"++y) "" (tolist dict))
-
-convertScore :: Maybe Integer -> Integer
-convertScore Nothing = 0
-convertScore (Just score) = score
-
-getHighscore :: [Char] -> Dict [Char] Integer -> Integer
-getHighscore user dict = convertScore (getval user dict)
-
 go :: IO Integer
 go =
     do
@@ -77,6 +20,7 @@ go =
         dict <- loadDict
         main (fixdel line) dict
 
+-- main menu
 menu = "\nMenu\n1 - New game\n2 - New challenge game\n3 - Display leaderboard\n\nEnter a menu item or 'q' to quit"
 
 main :: [Char] -> Dict [Char] Integer -> IO Integer
@@ -179,21 +123,89 @@ playChallenge (EndOfGameChallenge (ChallengeState board moves tiles index) won)
         putStrLn "Game over"
         return index
 
--- leaderboard dict shows a leaderboard with the top usernames and scores
+
+------------ Board Functions -------------
+
+emptyboard = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+
+-- initboard creates a new board with 2 random tiles
+initboard :: IO [[Integer]]
+initboard = 
+    do
+        num <- randomRIO (1, 2 :: Integer)
+        return (addTile (addTile emptyboard 1) num)
+
+-- initboardchallenge board n m s creates a new challenge board
+-- with n normal tiles, m movable tiles, and s static tiles 
+initboardchallenge :: [[Integer]] -> [Integer] -> Integer -> Integer -> IO [[Integer]]
+initboardchallenge board n m s 
+    | n /= [] = initboardchallenge (addTile board (head n)) (tail n) m s
+    | m > 0 = initboardchallenge (addTile board (-1)) n (m-1) s
+    | s > 0 = initboardchallenge (addTile board (-2)) n m (s-1)
+    | otherwise = return board
+
+-- display b prints the board with proper spacing
+display :: [[Integer]] -> String
+display b = "\n" ++ foldr (\ x y -> foldr displaynum "" x ++ "\n" ++ y) "\n" b
+
+-- helper for display - displaynum n s appends n to s with spacing
+displaynum :: Integer -> String -> String
+displaynum n s
+    | n == 0 = "*    " ++ s
+    | n < 10 = show n ++ "    " ++ s
+    | n < 100 = show n ++ "   " ++ s
+    | n < 1000 = show n ++ "  " ++ s
+    | otherwise = show n ++ " " ++ s
+
+
+------------ Dict Functions -------------
+
+-- leaderboard dict shows a leaderboard with the top scores in dict
 leaderboard :: Dict [Char] Integer -> [Char]
 leaderboard dict
     | null (tolist dict) = "\nno scores to display"
     | otherwise = "\nLeaderboard\n" ++ foldr showpair "" (top5 dict)
 
--- showpair (k,v) r formats the pair and appends it to r
+-- helper for leaderboard - showpair (k,v) r formats the pair and appends it to r
 showpair :: ([Char], Integer) -> [Char] -> [Char]
 showpair (k,v) r = k ++ ": " ++ show v ++ "\n" ++ r
 
--- sortByScore dict returns a sorted list of pairs with highest 5 scores in dict
+-- top5 dict returns a sorted list of pairs with highest 5 scores in dict
 top5 :: Dict [Char] Integer -> [([Char], Integer)]
 top5 dict = take 5 (sortBy (\ (_,v1) (_,v2) -> compare v2 v1) (tolist dict))
 
--- note: the following function was taken from the Assignment 3 solutions
+-- loadDict reads the dictionary from scores.txt
+loadDict :: IO (Dict [Char] Integer)
+loadDict =
+    do
+        scoresExist <- doesFileExist "scores.txt"
+        if scoresExist
+            then do
+                contents <- readFile "scores.txt"
+                let pairs = makepairs (lines contents)
+                return (foldr (uncurry insertval) emptyDict pairs)
+            else
+                return emptyDict
+
+-- helper for loadDict - makepairs lst returns a list of key value pairs in lst
+makepairs :: [[Char]] -> [([Char], Integer)]
+makepairs [] = []
+makepairs (k:v:t) = (k, read v) : makepairs t
+
+-- saveDict writes the dictionary to scores.txt
+saveDict :: Dict [Char] Integer -> IO ()
+saveDict dict = writeFile "scores.txt" (foldr (\ (k,v) y -> k++"\n"++show v++"\n"++y) "" (tolist dict))
+
+-- getHighscore user dict returns user's highscore
+getHighscore :: [Char] -> Dict [Char] Integer -> Integer
+getHighscore user dict = convertScore (getval user dict)
+
+-- helper for getHighscore
+convertScore :: Maybe Integer -> Integer
+convertScore Nothing = 0
+convertScore (Just score) = score
+
+-- note: fixdel is from the Assignment 3 solutions
 -- fixdel removes deleted elements from string
 fixdel :: [Char] -> [Char]
 fixdel st
@@ -204,7 +216,9 @@ remdel ('\DEL':r) = r
 remdel (a:'\DEL':r) = r
 remdel (a:r) = a: remdel r
 
--- testing values
+
+------------ Testing Values -------------
+
 test_dict = insertval "p1" 1234 (insertval "p2" 5432 (insertval "p3" 2111 emptyDict))
 bw = [[1024,1024,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 bl = [[4,32,256,8],[16,4,64,4],[4,32,16,8],[2,8,4,0]]
